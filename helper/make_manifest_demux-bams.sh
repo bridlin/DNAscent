5aa8e256bb0da92a4b487dfbbf8010dc72f1a97c
bridlin <bridlinbarckmann@gmail.com>
2026-02-12 12:53:21 +0100

    sizse down array size

--- helper/make_manifest_demux-bams.sh
+++ helper/make_manifest_demux-bams.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Inputs
 demux_dir="$output_dir/demux/"
-echo "demux_dir = $demux_dir"
+#echo "demux_dir = $demux_dir"
 
 # For alignment array
 ls "$demux_dir"/*.bam > "$output_dir/demux_list.txt"
