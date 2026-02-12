f43edd9d846e4207572d6d574ccd493a7e33ca37
bridlin <bridlinbarckmann@gmail.com>
2026-02-12 13:19:22 +0100

    still syntax error

--- step3_dnascent_array.sh
+++ step3_dnascent_array.sh
@@ -43,7 +43,7 @@ if [[ ! -f "$dnascent_index_dir/.built.ok" ]]; then
       "$container_sif" \
       DNAscent index  \
         --files /pod5  \
-        # --output /index
+        --output /index/pod5.index
     touch "$dnascent_index_dir/.built.ok"
   else
     echo "Waiting for DNAscent index ..."
