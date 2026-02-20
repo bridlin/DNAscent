bc15c0d23f5df42e22105d5120ccca0a1e634096
bridlin <bridlinbarckmann@gmail.com>
2026-02-20 13:22:48 +0100

    file error in bdg to bw converstion

--- run_detect_to_bigwig.sh
+++ run_detect_to_bigwig.sh
@@ -4,8 +4,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
 #SBATCH --partition=fast
-#SBATCH -o main_%j.out
-#SBATCH -e main_%j.err
+#SBATCH -o main_%A_%a.out
+#SBATCH -e main_%A_%a.err
 #SBATCH --mail-type=END
 #SBATCH --mail-user=bridlin.barckmann@umontpellier.fr
 
@@ -65,34 +65,34 @@ echo "Transforming DNAscent detect to bigwig files"
 echo "Submitting detect to bigwig array...Task ID: ${SLURM_ARRAY_TASK_ID}"
 
 # ---- Step 1: DNAscent detect -> bedGraph (call your Python) ----
-if [[ ! -s "$RAW_BDG_BRDU" ]]; then
-  echo "Generating bedGraph from DNAscent detect: $DETECT_INPUT -> $BRDU , $EDU, $BRDU_STRICT and $EDU_STRICT"
-  python  scripts/DNAscent/detect_to_bdg.py "$DETECT_INPUT" \   
-else
-  echo "RAW_BDG exists, skipping generation: $RAW_BDG_BRDU"
-fi
+# if [[ ! -s "$RAW_BDG_BRDU" ]]; then
+#   echo "Generating bedGraph from DNAscent detect: $DETECT_INPUT -> $BRDU , $EDU, $BRDU_STRICT and $EDU_STRICT"
+#   python  scripts/DNAscent/detect_to_bdg.py "$DETECT_INPUT" \   
+# else
+#   echo "RAW_BDG exists, skipping generation: $RAW_BDG_BRDU"
+# fi
 
 
-# Move all 4 files into BDG_DIR
-mv "$BRDU" "$BDG_DIR/"
-mv "$EDU" "$BDG_DIR/"
-mv "$BRDU_STRICT" "$BDG_DIR/"
-mv "$EDU_STRICT" "$BDG_DIR/"
+# # Move all 4 files into BDG_DIR
+# mv "$BRDU" "$BDG_DIR/"
+# mv "$EDU" "$BDG_DIR/"
+# mv "$BRDU_STRICT" "$BDG_DIR/"
+# mv "$EDU_STRICT" "$BDG_DIR/"
 
 
 
-# ---- Step 2: Ensure bedGraph is sorted and valid ----
-# Sort by chrom and start; force LC_ALL=C for speed and consistent collation.
-# EdU
-if [[ ! -s "$SORTED_BDG_EDU" ]]; then
-  echo "Sorting bedGraph..."
-  LC_ALL=C sort -k1,1 -k2,2n "$RAW_BDG_EDU" > "$SORTED_BDG_EDU"
-fi
-# BrdU
-if [[ ! -s "$SORTED_BDG_EDU" ]]; then
-  echo "Sorting bedGraph..."
-  LC_ALL=C sort -k1,1 -k2,2n "$RAW_BDG_BRDU" > "$SORTED_BDG_EDU"
-fi
+# # ---- Step 2: Ensure bedGraph is sorted and valid ----
+# # Sort by chrom and start; force LC_ALL=C for speed and consistent collation.
+# # EdU
+# if [[ ! -s "$SORTED_BDG_EDU" ]]; then
+#   echo "Sorting bedGraph..."
+#   LC_ALL=C sort -k1,1 -k2,2n "$RAW_BDG_EDU" > "$SORTED_BDG_EDU"
+# fi
+# # BrdU
+# if [[ ! -s "$SORTED_BDG_EDU" ]]; then
+#   echo "Sorting bedGraph..."
+#   LC_ALL=C sort -k1,1 -k2,2n "$RAW_BDG_BRDU" > "$SORTED_BDG_EDU"
+# fi
 
 
 
@@ -142,8 +142,8 @@ echo "[$(date)] DONE EDU sample=$SAMPLE"
 
 # BrdU
 if [[ ! -s "$BW_OUT_BRDU" ]]; then
-  echo "Converting to bigWig: $BDG_FOR_BW_EDU -> $BW_OUT_BRDU"
-  bedGraphToBigWig "$BDG_FOR_BW_EDU" "$CHROM_SIZES" "$BW_OUT_BRDU"
+  echo "Converting to bigWig: $BDG_FOR_BW_BRDU -> $BW_OUT_BRDU"
+  bedGraphToBigWig "$BDG_FOR_BW_BRDU" "$CHROM_SIZES" "$BW_OUT_BRDU"
 else
   echo "BigWig exists, skipping: $BW_OUT_BRDU"
 fi
